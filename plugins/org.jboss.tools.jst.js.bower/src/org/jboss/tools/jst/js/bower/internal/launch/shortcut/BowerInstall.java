dde318d74c4ddadf3e0766f2a7cbcb769a205165
Ilya Buziuk <ibuziuk@exadel.com>
2015-11-10 14:26:33 +0300

    Adjusting headers for Eclipse CQ process

--- plugins/org.jboss.tools.jst.js.bower/src/org/jboss/tools/jst/js/bower/internal/launch/shortcut/BowerInstall.java
+++ plugins/org.jboss.tools.jst.js.bower/src/org/jboss/tools/jst/js/bower/internal/launch/shortcut/BowerInstall.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat, Inc.
- * Distributed under license by Red Hat, Inc. All rights reserved.
- * This program is made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2015 Red Hat, Inc. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- *  Contributors:
- *       Red Hat, Inc. - initial API and implementation
+ * 	Contributors:
+ * 		 Red Hat Inc. - initial API and implementation and/or initial documentation
  *******************************************************************************/
 package org.jboss.tools.jst.js.bower.internal.launch.shortcut;
 
